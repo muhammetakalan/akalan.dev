1a81fdc2b18e010386e80d98338457b75f4daab2
Muhammet Emin Akalan <05akalan57@gmail.com>
2023-10-27 16:02:42 +0300

    add logo to body interface and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center font-light text-4xl">
-      COMING SOON
+    <div className="h-full flex flex-col justify-center items-center">
+      <div className="h-24">
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 0L0 24L5.71875 19.5177H9.46875L7.21875 16.5292L12 6.06987L16.7813 16.5292L14.5313 19.5177H18.2813L24 24L12 0ZM7.96875 16.5292L12 21.8522L16.0313 16.5292L12 19.5177L7.96875 16.5292Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
+      <h1 className="font-bold mt-4">COMING SOON</h1>
     </div>
   )
 }
