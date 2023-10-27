d82bd178c6fb299ec728190ca6d0be7b8061774a
Muhammet Emin Akalan <05akalan57@gmail.com>
2023-10-27 17:33:48 +0300

    style: create header interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-'use client'
-
 export default function Home() {
-  return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <div className="h-24">
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12 0L0 24L5.71875 19.5177H9.46875L7.21875 16.5292L12 6.06987L16.7813 16.5292L14.5313 19.5177H18.2813L24 24L12 0ZM7.96875 16.5292L12 21.8522L16.0313 16.5292L12 19.5177L7.96875 16.5292Z"
-            fill="currentColor"
-          />
-        </svg>
-      </div>
-      <h1 className="mt-4 font-bold">COMING SOON</h1>
-    </div>
-  )
+  return <div>Ana Sayfa</div>
 }
