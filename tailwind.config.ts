f698208981b71cc3a7ef178ca2d26430f0c2cf29
Muhammet Emin Akalan <muhammetakalan@proton.me>
2023-12-19 21:32:46 +0300

    chore(deps): bump package versions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 module.exports = {
   darkMode: ['class'],
-  content: ['src/app/**/*.{ts,tsx}', 'src/components/**/*.{ts,tsx}'],
+  content: [
+    'src/app/**/*.{ts,tsx,mdx}',
+    'src/components/**/*.{ts,tsx}',
+    'content/*.mdx'
+  ],
   theme: {
     container: {
       center: true,
@@ -62,11 +66,22 @@ module.exports = {
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' }
+        },
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' }
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'waving-hand': 'wave 2s linear infinite'
       }
     }
   },
